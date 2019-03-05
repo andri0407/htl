8453fed7e88dc8a308c309228502677025df2212
Ikhsan <thisembiring@gmail.com>
2019-03-05 15:55:45 +0700

    tambah method add di test2.php

--- application/controllers/Test2.php
+++ application/controllers/Test2.php
@@ -1,8 +1,14 @@
 <?php
 class Test2 extends CI_Controller{
-    public index(){
+    public function index(){
         return "Test";
     }
+
+    public function add(){
+        return "berhasil ditambah";
+    }
+
+    
 }
 
 ?>
